573de01f8a13b00c98af9111d51c5aee6391a267
Administrateur <Administrateur@SRV2016>
2018-10-12 11:15:32 +0200

    Ajout TP4 (CSOM) But du TP : ajout d'un élément dans une liste

---
+++ TP4.ps1
@@ -0,0 +1,18 @@
+﻿#CSOM creation d'un element
+# But du TP : ajout d'un élément dans une liste
+$context=New-Object Microsoft.Sharepoint.Client.ClientContext("http://srv2016")
+#$context.Credentials=New-Object System.net.NetworkCredentials("DM", "Adminstrateur", "*Ckv43#")
+$web=$context.Web
+
+$list=$web.Lists.GetByTitle("TP1")
+
+
+$ListItemCreationInformation = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$NewItem=$list.AddItem($ListItemCreationInformation)
+$NewItem['Title']='tutu'
+$NewItem["Y_x0020_A_x0020_DES_x0020_ESPACE"]="cui cui"
+
+
+$NewItem.Update()
+
+$context.ExecuteQuery()
